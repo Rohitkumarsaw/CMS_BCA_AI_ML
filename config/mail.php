37883976ddbb269cc2e32e69bfef1e5aafcd4658
Rohitkumarsaw <dellofficial795@gmail.com>
2026-06-21 18:08:45 +0530

    fix: use correct Gmail credentials for SMTP auth

--- config/mail.php
+++ config/mail.php
@@ -1,8 +1,8 @@
 <?php
 define('MAIL_HOST', 'smtp.gmail.com');
 define('MAIL_PORT', 587);
-define('MAIL_USERNAME', 'cms.notify.sitm@gmail.com');
-define('MAIL_PASSWORD', 'gpxz qkno zpfe cwae');
-define('MAIL_FROM', 'cms.notify.sitm@gmail.com');
+define('MAIL_USERNAME', 'rk7736806@gmail.com');
+define('MAIL_PASSWORD', 'gpxzqknozpfecwae');
+define('MAIL_FROM', 'rk7736806@gmail.com');
 define('MAIL_FROM_NAME', 'CMS BCA AI/ML');
 define('MAIL_TO', 'rk7736806@gmail.com');
